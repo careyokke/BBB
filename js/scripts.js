66f4fff12c3ddb4e0141fd715b55f2f3f2911cd5
Yokke Hargono <Careyokke@yahoo.com>
2017-06-09 10:53:49 -0700

    Add pic

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("#play").click(function() {
+  $("#show-results").show();
+    var userInput = parseInt(prompt("Enter any number to find Bing Bong OR BINGBONG!"));
+    for (var i=1; i<=userInput; i++) {
+      if ((i%3)===0 && (i%5)===0) {
+        $(".bingbongOutput ul").append("<li>!*BINGBONG*!</li>");
+      } else if ((i%3)===0 && (i%5)!==0) {
+          $(".bingbongOutput ul").append("<li>*bing*</li>");
+        } else if ((i%3)!==0 && (i%5)===0) {
+            $(".bingbongOutput ul").append("<li>*bong*</li>");
+          } else {
+              $(".bingbongOutput ul").append("<li>" + (i) + "</li>");
+            }
+    }
+    event.preventDefault();
+  });
+  $("#play-again").click(function(event) {
+    location.reload(true);
+
+  });
+});
