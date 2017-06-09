7a87fe766d8250eaf4d94b40ce51e8dde170bc8a
Yokke Hargono <Careyokke@yahoo.com>
2017-06-09 10:58:24 -0700

    if number is 5  would be BONG

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     var userInput = parseInt(prompt("Enter any number to find Bing Bong OR BINGBONG!"));
     for (var i=1; i<=userInput; i++) {
       if ((i%3)===0 && (i%5)===0) {
-        $(".bingbongOutput ul").append("<li>!*BINGBONG*!</li>");
+        $(".bingbongOutput ul").append("<li>!*BINGBONG!</li>");
       } else if ((i%3)===0 && (i%5)!==0) {
           $(".bingbongOutput ul").append("<li>*bing*</li>");
+        } else if ((i%3)!==0 && (i%5)===0) {
+            $(".bingbongOutput ul").append("<li>*bong*</li>");
