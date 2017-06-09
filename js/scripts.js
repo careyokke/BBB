4588bffda46719adb4599e734d9676b5047682b1
Yokke Hargono <Careyokke@yahoo.com>
2017-06-09 10:57:57 -0700

    if number is 3 not 5  would be BING

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,5 @@ $(document).ready(function() {
     for (var i=1; i<=userInput; i++) {
       if ((i%3)===0 && (i%5)===0) {
         $(".bingbongOutput ul").append("<li>!*BINGBONG*!</li>");
+      } else if ((i%3)===0 && (i%5)!==0) {
+          $(".bingbongOutput ul").append("<li>*bing*</li>");
